44b69386bb98bc47a3e79749b2825c7d86e60646
doubleboom <512316614@qq.com>
2022-08-08 01:02:47 +0800

    send file:show directly or download

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import type { DataConnection, Peer } from 'peerjs'
-// import pkg from 'peerjs'
-// const { Peer } = pkg
-// import { Peer } from 'peerjs'
+export interface ReceiveData {
+  file: Blob
+  type: string
+  size: string
+  filename: string
+}
+
 // const user = useUserStore()
 // const name = $ref(user.savedName)
-let peerid = $ref<string>('')
-let destpeerid = $ref<string>('')
+let peerId = $ref<string>('')
+let isConnect = $ref<boolean>(false)
+let destPeerId = $ref<string>('')
 const video = ref<HTMLVideoElement | null>(null)
+const file = ref<HTMLInputElement | null>(null)
 // const src = $ref('')
 let message = $ref<string>('')
 const messages = $ref<string[]>([])
@@ -30,36 +36,61 @@ const stop = () => {
     video.value.srcObject = null
 }
 
+const onData = (data: any) => {
+  if (data === 'close stream') { stop() }
+  else if (typeof data === 'object' && data.type) {
+    const newData: ReceiveData = data
+    const blob = new Blob([newData.file], { type: newData.type })
+    const url = URL.createObjectURL(blob)
+    if (newData.type.includes('image')) {
+      messages.push(`<img src='${url}' w-250px inline-block />`)
+    }
+    else if (newData.type.includes('video')) {
+      messages.push(`<video src='${url}' w-350px inline-block controls />`)
+    }
+    else {
+      const a = document.createElement('a')
+      a.href = url
+      a.download = data.filename
+      document.body.appendChild(a)
+      a.click()
+      a.remove()
+      URL.revokeObjectURL(url)
+      // console.log(url)
+      messages.push(`[note]:accept new file ${data.filename} size ${data.size}`)
+    }
+  }
+  else { messages.push(`other:${data}`) }
+}
+
 onMounted(async () => {
   if (typeof window !== 'undefined') {
     const { Peer } = await import('peerjs')
-    peerid = `test_peer_${Date.now()}`
-    messages.push(`init local peerid${peerid}`)
-    peer = new Peer(peerid)
-    peer?.on('connection', (lconn) => {
-      conn = lconn
-      destpeerid = lconn.peer
-
-      lconn.on('data', (data) => {
-        // Will print 'hi!'
-        if (data === 'close stream')
-          stop()
-
-        else
-          messages.push(`other:${data}`)
+    peerId = `test_peer_${Date.now()}`
+    messages.push(`[note]init local peerId ${peerId}`)
+    peer = new Peer(peerId)
+    peer?.on('connection', (remoteCon) => {
+      conn = remoteCon
+      destPeerId = remoteCon.peer
+      isConnect = true
+      remoteCon.on('data', (data: any) => {
+        onData(data)
       })
-      lconn.on('open', () => {
-        messages.push('connected!')
+      remoteCon.on('open', () => {
+        messages.push('[note]connected!')
       })
     })
     peer?.on('disconnected', (currentId) => {
-      messages.push('disconnected')
+      isConnect = false
+      messages.push('[note]disconnected')
     })
     peer?.on('close', () => {
       messages.push('close')
     })
     peer?.on('error', (err) => {
-      messages.push(err.message)
+      console.log(err)
+
+      messages.push(`[error]${err.message}`)
     })
 
     peer.on('call', (call) => {
@@ -79,7 +110,7 @@ onMounted(async () => {
         })
       })
       call.on('error', (err) => {
-        console.log(`remotestream err:${err}`)
+        console.log(`[error]remote stream err:${err.message}`)
       })
       call.on('close', () => {
         messages.push('[note]close stream!')
@@ -88,6 +119,34 @@ onMounted(async () => {
       })
     })
   }
+  file.value?.addEventListener('change', (e) => {
+    const target = e.target as HTMLInputElement
+    const files = target.files
+
+    if (!files)
+      return
+    const firstFile = files[0]
+    const blob = new Blob([firstFile], { type: firstFile.type || 'application/octet-stream' })
+    console.log(blob)
+
+    let sOutput = `${firstFile.size} bytes`
+    const nBytes = firstFile.size
+
+    // optional code for multiples approximation
+    const aMultiples = ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+    for (let nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++)
+      sOutput = `${nApprox.toFixed(3)} ${aMultiples[nMultiple]} (${nBytes} bytes)`
+
+    conn?.send({
+      file: blob,
+      filename: firstFile.name,
+      type: firstFile.type || 'application/octet-stream',
+      size: sOutput,
+    })
+    if (file.value)
+      file.value.value = ''
+    messages.push(`[note]send file:name:${firstFile.name},size:${sOutput}`)
+  })
 })
 // const go = () => {
 //   if (name)
@@ -101,19 +160,15 @@ const connect = () => {
   if (!peer)
     return
   conn = peer.connect(message)
-  destpeerid = message
+  destPeerId = message
   message = ''
   conn?.on('open', () => {
-    messages.push('connected!')
-    // conn?.send('hi!')
+    isConnect = true
+    messages.push('[note]connected!')
   })
-  conn?.on('data', (data) => {
+  conn?.on('data', (data: any) => {
     // Will print 'hi!'
-    if (data === 'close stream')
-      stop()
-
-    else
-      messages.push(`other:${data}`)
+    onData(data)
   })
 }
 
@@ -121,21 +176,23 @@ const sendMessage = () => {
   console.log(message)
   if (message === 'close stream')
     stop()
+  if (!message)
+    return
   conn?.send(message)
   messages.push(`me:${message}`)
   message = ''
 }
 
-const startAudio = () => {
-  console.log('startVideo', peerid, destpeerid)
-  if (!destpeerid)
+const startVideo = () => {
+  console.log('startVideo', peerId, destPeerId)
+  if (!destPeerId)
     return
   navigator.mediaDevices.getDisplayMedia(displayMediaOptions).then(
     (stream) => {
-      console.log('localstream')
+      console.log('local stream')
       if (!peer)
         return
-      const call = peer.call(destpeerid, stream)
+      const call = peer.call(destPeerId, stream)
       if (!video.value)
         return
       video.value.srcObject = stream
@@ -148,7 +205,7 @@ const startAudio = () => {
       })
       call.on('error', (err) => {
         // Show stream in some <video> element.
-        console.log(`localstream error:${err}`)
+        console.log(`local stream error:${err}`)
         // src = URL.createObjectURL(remoteStream)
       })
       call.on('close', () => {
@@ -183,6 +240,9 @@ const getVideoInfo = () => {
   console.info('Track constraints:')
   console.info(JSON.stringify(videoTrack.getConstraints(), null, 2))
 }
+const sendFile = () => {
+  file.value?.click()
+}
 
 const { t } = useI18n()
 </script>
@@ -193,13 +253,17 @@ const { t } = useI18n()
       <div i-carbon-campsite inline-block />
     </div>
     <div>
-      <span>{{ peerid }}</span>
+      <div un-v-text-bottom inline-block :class="{ 'i-carbon-unlink': !isConnect, 'i-carbon-link': isConnect }" />
+      {{ destPeerId }}
     </div>
+    <!-- <div>
+      <span>{{ peerid }}</span>
+    </div> -->
     <input
       id="input"
       v-model="message"
-      placeholder="输入房间号"
-      aria-label="输入房间号"
+      :placeholder="isConnect ? '发点啥' : '输入房间号'"
+      :aria-label="isConnect ? '发点啥' : '输入房间号'"
       type="text"
       autocomplete="false"
       p="x4 y2"
@@ -208,18 +272,41 @@ const { t } = useI18n()
       bg="transparent"
       border="~ rounded gray-200 dark:gray-700"
       outline="none active:none"
-      @keydown.enter="conn ? sendMessage() : connect()"
     >
     <ul>
       <li v-for="(item, index) in messages" :key="index">
-        {{ item }}
+        <span v-html="item" />
       </li>
     </ul>
     <button
-      btn m-3 text-sm
-      @click="startAudio"
+      v-if="isConnect" btn m-3
+      text-sm
+      @click="sendMessage"
     >
-      {{ t('button.go') }}
+      send
+    </button>
+    <button
+      v-else btn m-3
+      text-sm
+      @click="connect"
+    >
+      connect
+    </button>
+    <button
+      v-if="isConnect"
+      btn m-3
+      text-sm
+      @click="startVideo"
+    >
+      start video
+    </button>
+    <button
+      v-if="isConnect"
+      btn m-3
+      text-sm
+      @click="sendFile"
+    >
+      send file
     </button>
     <button
       btn m-3 text-sm
@@ -227,7 +314,10 @@ const { t } = useI18n()
     >
       getinfo
     </button>
-    <video ref="video" controls />
+    <div>
+      <video ref="video" inline-block controls />
+    </div>
+    <input ref="file" type="file" display-none>
     <!-- <p>
       <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
         Vitesse
